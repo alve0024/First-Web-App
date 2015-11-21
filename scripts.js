566ff657e164f5496570c710bcb2f14774a92517
alve0024 <alve0024@algonquinlive.com>
2015-11-20 20:53:19 -0500

    Turn the code generic

--- scripts.js
+++ scripts.js
@@ -1,73 +1,25 @@
 
 $(document).ready(function(ev){
-var steps = ["#step-one", "#step-two", "#step-three", "#step-four", "#step-five",
-						 "#step-six", "#step-seven", "#step-eight", "#step-nine", "#step-ten",
-						 "#step-eleven", "#step-twelve"];
-	
+
+	$('.tutorialstep').on('pageshow', function(ev, data) {
+    var step = $(data.toPage).attr('id');
+    localStorage['currentStep'] = '#'+step;
+	});				 
+
 	$('#intro').on('pageshow', function(ev, data) {
-		var currentStep = localStorage['currentStep'];
-		console.log(currentStep);
+		currentStep = localStorage['currentStep'];
+
 		if (currentStep) {
-			$('#btnContinue').removeClass('ui-state-disabled');
-			$('#btnContinue').attr('href', currentStep);
-			console.log($('#btnContinue').attr('href'));
+			$('#btnStart').html('Continue...');
+			$('#btnStart').attr('href', currentStep);
 		}	else {
-			$('#btnContinue').addClass('ui-state-disabled');
+			$('#btnStart').html('Start');
+			$('#btnStart').attr('href', '#step-one');
 		}
 	});
 
 	$('#clearLocalStorage').on('click', function() {
 		localStorage.removeItem('currentStep');
-		console.log('localStorage removed!');
 	});
-
-	$(steps[0]).on('pageshow', function(ev, data) {
-		localStorage['currentStep'] = steps[0];
-	});
-
-	$(steps[1]).on('pageshow', function(ev, data) {
-		localStorage['currentStep'] = steps[1];
-	});
-
-	$(steps[2]).on('pageshow', function(ev, data) {
-		localStorage['currentStep'] = steps[2];
-	});
-
-	$(steps[3]).on('pageshow', function(ev, data) {
-		localStorage['currentStep'] = steps[3];
-	});
-
-	$(steps[4]).on('pageshow', function(ev, data) {
-		localStorage['currentStep'] = steps[4];
-	});
-
-	$(steps[5]).on('pageshow', function(ev, data) {
-		localStorage['currentStep'] = steps[5];
-	});
-
-	$(steps[6]).on('pageshow', function(ev, data) {
-		localStorage['currentStep'] = steps[6];
-	});
-
-	$(steps[7]).on('pageshow', function(ev, data) {
-		localStorage['currentStep'] = steps[7];
-	});
-
-	$(steps[8]).on('pageshow', function(ev, data) {
-		localStorage['currentStep'] = steps[8];
-	});
-
-	$(steps[9]).on('pageshow', function(ev, data) {
-		localStorage['currentStep'] = steps[9];
-	});
-
-	$(steps[10]).on('pageshow', function(ev, data) {
-		localStorage['currentStep'] = steps[10];
-	});
-
-	$(steps[11]).on('pageshow', function(ev, data) {
-		localStorage['currentStep'] = steps[11];
-	});
-
 });
 
